048bc7113e08da0ebddb87c1c30769dac87e1249
yyytae0 <yyytae0@gmail.com>
2024-03-25 09:43:47 +0900

    Feat: set router and roulette page

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
 import './App.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Entry from './pages/Entry';
 
 function App() {
 
   return (
-    <>
-      <Entry/>
-    </>
+    <Router>
+      <Routes>
+        <Route path='/' element={<Entry/>}/>
+        <Route path='/roulette'/>
+      </Routes>
+    </Router>
   )
 }
 
---
+++ src/pages/Roulette.tsx
@@ -0,0 +1,12 @@
+
+
+const Roulette = () => {
+  return (
+    <div style={{width: '100svw', height: '100svh', display: 'flex', flexDirection: 'column', overflow: 'hidden'}}>
+      룰렛
+    </div>
+  )
+
+};
+
+export default Roulette;
