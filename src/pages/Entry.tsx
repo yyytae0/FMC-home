bd05d76c70b0a17ca2ceb08a91c8f8e28b926bec
yyytae0 <yyytae0@gmail.com>
2024-03-25 09:20:11 +0900

    Feat: set initial page css on entry page

--- src/pages/Entry.tsx
+++ src/pages/Entry.tsx
@@ -2,7 +2,11 @@
 
 const Entry = () => {
   return (
-    <></>
+    <div style={{width: '100svw', height: '100svh', display: 'flex', flexDirection: 'column', overflow: 'hidden'}}>
+      <div style={{border: '1px solid black', flexGrow: 1}}>
+        <h1>우리가 남이가</h1>
+      </div>
+    </div>
   )
 
 };
